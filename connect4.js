33d53ca319a7e1cadc58aef25f8085c35c7cdf32
CompSciGuy_IT <compsciguy.it@gmail.com>
2018-01-04 18:49:16 +1000

    Made code more modular by splitting it into more functions reallocated ids in the table for better targeting working on improving game logic

--- connect4.js
+++ connect4.js
@@ -11,6 +11,7 @@ var columns = [col_1, col_2, col_3, col_4, col_5, col_6, col_7];
 
 var button_text = $('#btn').text();
 var player_1_turn = true;
+var is_won = false;
 
 var player_1 = {
     name: '',
@@ -24,7 +25,7 @@ var player_2 = {
 
 /* Prompt player's name */
 function prompt_player_name(player) {
-    if (player.color === 'red') {
+    if (player.color === 'blue') {
         player.name = prompt('Player One Enter Your Name.  You Will Be Red.');
     } else {
         player.name = prompt('Player Two Enter Your Name.  You Will Be Blue.');
@@ -63,35 +64,57 @@ function unchange_column_colour() {
     });
 }
 
+/* Column is full animation */
+function column_full(column) {
+    setTimeout(() => {
+        columns[column].parent().css('background', 'rgb(255, 128, 0)');
+    }, 250);
+    setTimeout(() => {
+        columns[column].parent().css('background', 'grey');
+    }, 250);
+    setTimeout(() => {
+        columns[column].parent().css('background', 'rgb(255, 128, 0)');
+    }, 250);
+    setTimeout(() => {
+        columns[column].parent().css('background', 'grey');
+    }, 250);
+    setTimeout(() => {
+        columns[column].parent().css('background', 'rgb(255, 128, 0)');
+    }, 250);
+}
+
 /* Check if top row in column is empty */
-function check_top_empty(space_id) {
+function check_space(space_id) {
     switch (space_id) {
         case 'row-1-col-1':
-            if ($('#row-1-col-1 div').hasClass('circle')) return true;
-            return false;
+            if (!($('#row-1-col-1').hasClass('circle-red')) || !($('#row-1-col-1').hasClass('circle-blue'))) {
+                return true;
+            } else {
+                return false;
+            }
     
         case 'row-1-col-2':
-            if ($('#row-1-col-2 div').hasClass('circle')) return true;
+            if (!($('#row-1-col-2').hasClass('circle-red')) || !($('#row-1-col-2').hasClass('circle-blue'))) return true;
             return false;
 
         case 'row-1-col-3':
-            if ($('#row-1-col-3 div').hasClass('circle')) return true;
+            if (!($('#row-1-col-3').hasClass('circle-red')) || !($('#row-1-col-3').hasClass('circle-blue'))) return true;
             return false;
 
         case 'row-1-col-4':
-            if ($('#row-1-col-4 div').hasClass('circle')) return true;
+            if (!($('#row-1-col-4').hasClass('circle-red')) || !($('#row-1-col-4').hasClass('circle-blue'))) return true;
             return false;
 
         case 'row-1-col-5':
-            if ($('#row-1-col-5 div').hasClass('circle')) return true;
+            if (!($('#row-1-col-5').hasClass('circle-red')) || !($('#row-1-col-5').hasClass('circle-blue'))) return true;
             return false;
 
         case 'row-1-col-6':
-            if ($('#row-1-col-6 div').hasClass('circle')) return true;
+            if (!($('#row-1-col-6').hasClass('circle-red')) || !($('#row-1-col-6').hasClass('circle-blue'))) return true;
             return false;
 
         case 'row-1-col-7':
-            if ($('#row-1-col-7 div').hasClass('circle')) return true;
+            if (!($('#row-1-col-7').hasClass('circle-red')) || !($('#row-1-col-7').hasClass('circle-blue'))) return true;
             return false;
     
         default:
@@ -103,42 +126,38 @@ function check_top_empty(space_id) {
 function place_chip(space_id) {
     switch (space_id) {
         case 'row-1-col-1':
-            $('#row-1-col-1 .chip-placed').addClass('circle-red');
-            $('#row-1-col-1 .chip-placed').addClass('fadeIn');
-            /*
-            setTimeout(function() {
-                $('#row-1-col-1 div').removeClass('circle');
-            }, 2000);*/
+            $('#row-1-col-1').addClass('circle-red');
+            $('#row-1-col-1').addClass('fadeIn');
             break;
     
         case 'row-1-col-2':
-            $('#row-1-col-2 .chip-placed').addClass('circle-red');
-            $('#row-1-col-2 .chip-placed').addClass('fadeIn');
+            $('#row-1-col-2').addClass('circle-red');
+            $('#row-1-col-2').addClass('fadeIn');
             break;
 
         case 'row-1-col-3':
-            $('#row-1-col-3 .chip-placed').addClass('circle-red');
-            $('#row-1-col-3 .chip-placed').addClass('fadeIn');
+            $('#row-1-col-3').addClass('circle-red');
+            $('#row-1-col-3').addClass('fadeIn');
             break;
 
         case 'row-1-col-4':
-            $('#row-1-col-4 .chip-placed').addClass('circle-red');
-            $('#row-1-col-4 .chip-placed').addClass('fadeIn');
+            $('#row-1-col-4').addClass('circle-red');
+            $('#row-1-col-4').addClass('fadeIn');
             break;
 
         case 'row-1-col-5':
-            $('#row-1-col-5 .chip-placed').addClass('circle-red');
-            $('#row-1-col-5 .chip-placed').addClass('fadeIn');
+            $('#row-1-col-5').addClass('circle-red');
+            $('#row-1-col-5').addClass('fadeIn');
             break;
 
         case 'row-1-col-6':
-            $('#row-1-col-6 .chip-placed').addClass('circle-red');
-            $('#row-1-col-6 .chip-placed').addClass('fadeIn');
+            $('#row-1-col-6').addClass('circle-red');
+            $('#row-1-col-6').addClass('fadeIn');
             break;
 
         case 'row-1-col-7':
-            $('#row-1-col-7 .chip-placed').addClass('circle-red');
-            $('#row-1-col-7 .chip-placed').addClass('fadeIn');
+            $('#row-1-col-7').addClass('circle-red');
+            $('#row-1-col-7').addClass('fadeIn');
             break;
 
         default:
@@ -146,53 +165,74 @@ function place_chip(space_id) {
     }
 }
 
-/* Get id of top row element */
-function get_top_id(column) {
-    return column[0].parent().attr('id');
-}
-
 /* Player selects column to drop chip */
 function select_column() {
     change_column_colour();
-    columns.forEach(column => {
-        column.click(function() {
-            var top_space_id = column[0].parentElement.id;
-            if (check_top_empty(top_space_id)) {
-                place_chip(top_space_id);
-            } else {
-                alert("didn't work");
-            }
-        })
-    });  
+    drop_chip();
 }
 
-/* Play Game */
-$('#btn').click(function() {
+/* Drop chip */
+function drop_chip() {
+    columns.forEach(column => {
+        column.click(function() {
+            var top_space_id = column[0].firstChild.id;
+            if (false) {
+                alert('empty');
+                place_chip(top_space_id);
+            } else {
+                alert('full');
+                column_full(column);
+            }
+        })
+    }); 
+}
+/* Begin Game */
+function begin_game() {
+    prompt_player_name(player_1);
+    prompt_player_name(player_2);
+    $('#btn').removeClass('btn-success');
+    $('#btn').addClass('btn-danger');
+    $('#btn').text('Reset');
+}
 
-    if (button_text === "Let's get started!") {
-        prompt_player_name(player_1);
-        prompt_player_name(player_2);
-        $(this).removeClass('btn-success');
-        $(this).addClass('btn-danger');
-        $(this).text('Reset');
-        button_text = 'Reset';
+/* Reset Game */
+function reset_game() {
+    $('#btn').removeClass('btn-danger');
+    $('#btn').addClass('btn-success');
+    $('#btn').text("Let's get started!");
+    unchange_column_colour();
+    clear_play_area(); 
+}
 
-        /* player turn */
-        if (player_1_turn) {
-            $('#feedback').text(player_1.name + ": It's your turn, please pick a column to drop your blue chip.");
-            select_column();
-        } else {
-            $('#feedback').text(player_2.name + ": It's your turn, please pick a column to drop your red chip.");
-        }
+/* Player Turn */
+function player_turn() {
+    if (player_1_turn) {
+        $('#feedback').text(player_1.name + ": It's your turn, please pick a column to drop your blue chip.");
+        select_column();
     } else {
-        $(this).removeClass('btn-danger');
-        $(this).addClass('btn-success');
-        $(this).text("Let's get started!");
-        button_text = "Let's get started!";
-        unchange_column_colour();
-        clear_play_area();    
+        $('#feedback').text(player_2.name + ": It's your turn, please pick a column to drop your red chip.");
     }
+}
 
+/******************************************* */
+/*                 Play Game                 */
+/******************************************* */
+$('#btn').click(function() {
+    if ($('#btn').text() === "Let's get started!") {
+        begin_game();
+        while (!(is_won)) {      // While game has not been won
+
+            // The next 4 lines are needed to keep the browserr from crashing
+            var win = prompt('win game y/n');
+            if (win === 'y') {
+                is_won = true;
+            }
+
+            player_turn(); 
+        }       
+    } else {
+        reset_game();
+    }
 })
 
 
