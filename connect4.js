604642370b24398814175277e2a6bf0332488884
CompSciGuy_IT <compsciguy.it@gmail.com>
2018-01-07 19:54:43 +1000

    Program breaks during begin_game function after player names are entered, game freezes button doesn't change colour or text

--- connect4.js
+++ connect4.js
@@ -9,6 +9,7 @@ var col_7 = $('.col-7');
 
 var columns = [col_1, col_2, col_3, col_4, col_5, col_6, col_7];
 
+var play = true;
 var button_text = $('#btn').text();
 var player_1_turn = true;
 var is_won = false;
@@ -36,7 +37,8 @@ function prompt_player_name(player) {
 
 /* Reset game data */
 function reset_game_data() {
-    player_1.name, player_2.name, player_1.score, player_2.score = '';
+    player_1.name, player_2.name = '';
+    player_1.score, player_2.score = 0;
 }
 
 /* Clear play area and player data*/
@@ -52,7 +54,11 @@ function clear_play_area() {
 function change_column_colour() {
     columns.forEach(column => {
         column.mouseenter(function() {
-            column.parent().css('background', 'rgba(0, 255, 0, 0.75)');
+            if (player_1_turn) {
+                column.parent().css('background', '#9999FF');
+            } else {
+                column.parent().css('background', '#FF9999');
+            }
         })
         
         column.mouseleave(function() {
@@ -93,38 +99,53 @@ function column_full(column) {
 function check_space(space_id) {
     switch (space_id) {
         case 'row-1-col-1':
-            if (!($('#row-1-col-1').hasClass('circle-red')) || !($('#row-1-col-1').hasClass('circle-blue'))) {
+            if (($('#row-1-col-1').is('circle-red')) || ($('#row-1-col-1').is('circle-blue'))) {
                 return true;
             } else {
                 return false;
             }
     
         case 'row-1-col-2':
-            if (!($('#row-1-col-2').hasClass('circle-red')) || !($('#row-1-col-2').hasClass('circle-blue'))) return true;
-            return false;
+            if (($('#row-1-col-2').hasClass('circle-red')) || ($('#row-1-col-2').hasClass('circle-blue'))) {
+                return true;
+            } else {
+                return false;
+            }
 
         case 'row-1-col-3':
-            if (!($('#row-1-col-3').hasClass('circle-red')) || !($('#row-1-col-3').hasClass('circle-blue'))) return true;
-            return false;
+            if (($('#row-1-col-3').hasClass('circle-red')) || ($('#row-1-col-3').hasClass('circle-blue'))) {
+                return true;
+            } else {
+                return false;
+            }
 
         case 'row-1-col-4':
-            if (!($('#row-1-col-4').hasClass('circle-red')) || !($('#row-1-col-4').hasClass('circle-blue'))) return true;
-            return false;
+            if (($('#row-1-col-4').hasClass('circle-red')) || ($('#row-1-col-4').hasClass('circle-blue'))) {
+                return true;
+            } else {
+                return false;
+            }
 
         case 'row-1-col-5':
-            if (!($('#row-1-col-5').hasClass('circle-red')) || !($('#row-1-col-5').hasClass('circle-blue'))) return true;
-            return false;
+            if (($('#row-1-col-5').hasClass('circle-red')) || ($('#row-1-col-5').hasClass('circle-blue'))) {
+                return true;
+            } else {
+                return false;
+            }
 
         case 'row-1-col-6':
-            if (!($('#row-1-col-6').hasClass('circle-red')) || !($('#row-1-col-6').hasClass('circle-blue'))) return true;
-            return false;
+            if (($('#row-1-col-6').hasClass('circle-red')) || ($('#row-1-col-6').hasClass('circle-blue'))) {
+                return true;
+            } else {
+                return false;
+            }
 
         case 'row-1-col-7':
-            if (!($('#row-1-col-7').hasClass('circle-red')) || !($('#row-1-col-7').hasClass('circle-blue'))) return true;
-            return false;
-    
-        default:
-            return false;
+            if (($('#row-1-col-7').hasClass('circle-red')) || ($('#row-1-col-7').hasClass('circle-blue'))) {
+                return true;
+            } else {
+                return false;
+            }
     }
 }
 
@@ -132,43 +153,103 @@ function check_space(space_id) {
 function place_chip(space_id) {
     switch (space_id) {
         case 'row-1-col-1':
-            $('#row-1-col-1').addClass('circle-red');
-            $('#row-1-col-1').addClass('fadeIn');
+            if (player_1_turn) {
+                $('#row-1-col-1').addClass('circle-blue');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            } else {
+                $('#row-1-col-1').addClass('circle-red');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            }
             break;
     
         case 'row-1-col-2':
-            $('#row-1-col-2').addClass('circle-red');
-            $('#row-1-col-2').addClass('fadeIn');
+            if (player_1_turn) {
+                $('#row-1-col-1').addClass('circle-blue');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            } else {
+                $('#row-1-col-1').addClass('circle-red');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            }
             break;
 
         case 'row-1-col-3':
-            $('#row-1-col-3').addClass('circle-red');
-            $('#row-1-col-3').addClass('fadeIn');
+            if (player_1_turn) {
+                $('#row-1-col-1').addClass('circle-blue');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            } else {
+                $('#row-1-col-1').addClass('circle-red');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            }
             break;
 
         case 'row-1-col-4':
-            $('#row-1-col-4').addClass('circle-red');
-            $('#row-1-col-4').addClass('fadeIn');
+            if (player_1_turn) {
+                $('#row-1-col-1').addClass('circle-blue');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            } else {
+                $('#row-1-col-1').addClass('circle-red');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            }
             break;
 
         case 'row-1-col-5':
-            $('#row-1-col-5').addClass('circle-red');
-            $('#row-1-col-5').addClass('fadeIn');
+            if (player_1_turn) {
+                $('#row-1-col-1').addClass('circle-blue');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            } else {
+                $('#row-1-col-1').addClass('circle-red');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            }
             break;
 
         case 'row-1-col-6':
-            $('#row-1-col-6').addClass('circle-red');
-            $('#row-1-col-6').addClass('fadeIn');
+            if (player_1_turn) {
+                $('#row-1-col-1').addClass('circle-blue');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            } else {
+                $('#row-1-col-1').addClass('circle-red');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            }
             break;
 
         case 'row-1-col-7':
-            $('#row-1-col-7').addClass('circle-red');
-            $('#row-1-col-7').addClass('fadeIn');
+            if (player_1_turn) {
+                $('#row-1-col-1').addClass('circle-blue');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            } else {
+                $('#row-1-col-1').addClass('circle-red');
+                $('#row-1-col-1').addClass('fadeIn');
+                player_1_turn = !player_1_turn;                
+            }
             break;
-
-        default:
-            return false;
     }
+}
+
+/* Drop chip */
+function drop_chip() {
+    columns.forEach(column => {
+        column.click(function() {
+            var top_space_id = column[0].firstChild.id;
+            if (check_space(top_space_id)) {
+                column_full(column);
+            } else {
+                place_chip(top_space_id);
+            }
+        })
+    }); 
 }
 
 /* Player selects column to drop chip */
@@ -177,21 +258,6 @@ function select_column() {
     drop_chip();
 }
 
-/* Drop chip */
-function drop_chip() {
-    columns.forEach(column => {
-        column.click(function() {
-            var top_space_id = column[0].firstChild.id;
-            if (false) {
-                alert('empty');
-                place_chip(top_space_id);
-            } else {
-                alert('full');
-                column_full(column);
-            }
-        })
-    }); 
-}
 /* Begin Game */
 function begin_game() {
     prompt_player_name(player_1);
@@ -206,6 +272,7 @@ function reset_game() {
     $('#btn').removeClass('btn-danger');
     $('#btn').addClass('btn-success');
     $('#btn').text("Let's get started!");
+    reset_game_data();
     unchange_column_colour();
     clear_play_area(); 
 }
@@ -217,30 +284,93 @@ function player_turn() {
         select_column();
     } else {
         $('#feedback').text(player_2.name + ": It's your turn, please pick a column to drop your red chip.");
+        select_column();
+    }
+}
+
+/* Prompt to play another round */
+function another_round() {
+    var another_round = prompt("Play another round? (y/n)");
+    switch (another_round) {
+        case 'y':
+        case 'Y':
+        case 'yes':
+        case 'YES':
+        case 'Yes':
+            return true;
+    
+        default:
+            return false;
     }
 }
 
 /******************************************* */
 /*                 Play Game                 */
 /******************************************* */
+
+// while (1) {
+//     reset_game_data();
+//     do {
+
+//         play = another_round();
+//     } while (play === true);
+// }
+
 $('#btn').click(function() {
     if ($('#btn').text() === "Let's get started!") {
-        reset_game_data();
         begin_game();
-        while (!(is_won)) {      // While game has not been won
+        do {
+            do {
+                if (player_1_turn) {
+                    do {
+                        player_turn();     
+                    } while (player_1_turn);        
+                } else {
+                    do {
+                        player_turn();     
+                    } while (!player_1_turn);
+                }        
+            } while (!(is_won));
+        
+            play = another_round();
+        } while (play === true);
 
-            // The next 4 lines are needed to keep the browserr from crashing
-            var win = prompt('win game y/n');
-            if (win === 'y') {
-                is_won = true;
-            }
 
-            player_turn(); 
-        }       
+        // do {
+
+        //     play = another_round();
+        // } while (play === true);
+
+        // while (!(is_won)) {      // While game has not been won
+
+        //     // The next 4 lines are needed to keep the browserr from crashing
+        //     var win = prompt('win game y/n');
+        //     if (win === 'y') {
+        //         is_won = true;
+        //     }
+
+        //     player_turn(); 
+        // }       
     } else {
         reset_game();
     }
-})
+});
+
+// do {
+//     do {
+//         if (player_1_turn) {
+//             do {
+//                 player_turn();
+//             } while (condition);        
+//         } else {
+//             do {
+//                 player_turn();
+//             } while (condition);
+//         }        
+//     } while (!(is_won));
+
+//     play = another_round();
+// } while (play === true);
 
 
 
